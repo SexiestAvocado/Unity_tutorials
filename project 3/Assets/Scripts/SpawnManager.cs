d81bc32c29cb4002bf7fd79d4bf5aef2e2d64a8f
SexiestAvocado <sexiestavocadoalive@gmail.com>
2021-11-22 19:17:35 -0600

    working still challenge 3

--- project 3/Assets/Scripts/SpawnManager.cs
+++ project 3/Assets/Scripts/SpawnManager.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
-    
+    public GameObject[] obstaclePrefabs;
+
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    private int whichObstacle;
+    private Vector3 spawnPosition = new Vector3(25, 0, 0);
+    private float timer;
+    private float timeBeforeNextObstacle;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +25,38 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void SpawnObstacle()
     {
         if (playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
-        }        
+            whichObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[whichObstacle], spawnPosition, obstaclePrefabs[whichObstacle].transform.rotation);
+            //1 chance out of 2 to spawn the same obstacle on top of the first one
+            if (Random.Range(0, 2) == 1)
+            {
+                GameObject instantiatedObject = obstaclePrefabs[whichObstacle];
+                float additionalHeight = instantiatedObject.GetComponent<BoxCollider>().size.y;
+                Instantiate(obstaclePrefabs[whichObstacle], new Vector3(spawnPosition.x, spawnPosition.y + additionalHeight, spawnPosition.z), obstaclePrefabs[whichObstacle].transform.rotation);
+                //1chance out of 6 ot have a 3stacked obstacle
+                if (Random.Range(0, 3) == 1)
+                {
+                    Instantiate(obstaclePrefabs[whichObstacle], new Vector3(spawnPosition.x, spawnPosition.y + 2 * additionalHeight, spawnPosition.z), obstaclePrefabs[whichObstacle].transform.rotation);
+                }
+            }
+            timer = Time.time;
+            timeBeforeNextObstacle = Random.Range(1.0f, 2.0f);
+        }
     }
+
+
 }
+    /*GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+    Vector3 spawnPosition = obstaclePrefab.transform.position;
+
+    if (playerControllerScript.gameOver == false)
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    } */
+    
