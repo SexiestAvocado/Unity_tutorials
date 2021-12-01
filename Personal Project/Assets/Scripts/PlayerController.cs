02805f2c41a676805f9c72bc5923c911a51af7cd
SexiestAvocado <sexiestavocadoalive@gmail.com>
2021-11-30 23:07:32 -0600

    created first 2d project

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 10.0f;
+    private float speed = 3.5f;
     private Rigidbody PlayerRb;
 
     // Start is called before the first frame update
@@ -20,7 +20,15 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        PlayerRb.AddForce(Vector3.forward * speed * verticalInput);
-        PlayerRb.AddForce(Vector3.right * speed * horizontalInput);
+        //PlayerRb.AddForce(Vector3.forward * speed * verticalInput);
+        //PlayerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        //transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (transform.position.z < 0 || transform.position.z > 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
     }
 }
